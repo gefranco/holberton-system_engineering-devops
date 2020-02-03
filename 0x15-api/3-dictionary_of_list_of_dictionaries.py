006661debf77550d0b36a7e7dba2f0fee6e37de7
gefranco <933@holbertonschool.com>
2020-02-03 23:55:39 +0000

    [add]3. Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+'''
+_____________________________________
+get TODO from all users in https://jsonplaceholder.typicode.com/
+and Export to JSON
+_____________________________________
+'''
+import json
+import requests
+import sys
+if __name__ == "__main__":
+
+    tasks_done = 0
+    total_tasks = 0
+    list_tasks_done = []
+    json_task = {}
+    rows_csv = []
+    user_json = {}
+    request = requests.get('https://jsonplaceholder.typicode.com/users')
+    users = json.loads(request.text)
+    for user in users:
+        if(True):
+            request = requests.get(
+                'https://jsonplaceholder.typicode.com/todos'
+            )
+
+            tasks = json.loads(request.text)
+            with open(
+                        "todo_all_employees.json",
+                        mode='w',
+                        encoding='utf-8') as f:
+                for task in tasks:
+                    row_csv = ()
+                    if(task.get('userId') == user.get('id')):
+                        json_task = {}
+                        json_task['task'] = task.get('title')
+                        json_task['completed'] = task.get('completed')
+                        json_task['username'] = user.get('username')
+                        list_tasks_done.append(json_task)
+                    user_json[user['id']] = list_tasks_done
+                json.dump(user_json, f)
