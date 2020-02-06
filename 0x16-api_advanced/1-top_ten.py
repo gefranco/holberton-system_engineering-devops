98e4322cf982876a2f54b79db89ee8243f0784cd
gefranco <933@holbertonschool.com>
2020-02-06 20:42:50 +0000

    [fix]

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,18 +13,17 @@ def top_ten(subreddit):
         request = requests.get(
             'https://www.reddit.com/r/'+sys.argv[1] +
             '/top.json?limit=10&raw_json=1',
+            allow_redirects=False,
             headers={'User-agent': 'gefrancof'}
         )
 
         dictionary = json.loads(request.text)
-
         data = dictionary['data']
         for data_index in data['children']:
             data_children = data_index['data']
             title = data_children['title']
-
-            print(title)
+            title = title.encode(encoding="utf-8")
+            sys.stdout.write(str(title)+"\n")
 
     except Exception as e:
-        print(e)
-        return 0
+        print('None')
