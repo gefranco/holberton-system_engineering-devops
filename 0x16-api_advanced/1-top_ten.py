8e668f4c3c324793f572abff26f88dfda3bdec1a
gefranco <933@holbertonschool.com>
2020-02-06 21:34:22 +0000

    [fix]

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 '''
+___________________
+module that contains def top_ten(subreddit) function
+___________________
 '''
 import json
 import requests
 import sys
 
+
 def top_ten(subreddit):
     '''
+    return the first 10 hot posts listed for a given subreddit.
     '''
     request = requests.get(
         'https://www.reddit.com/r/'+sys.argv[1] +
@@ -21,4 +26,5 @@ def top_ten(subreddit):
     data = dictionary['data']
     for data_index in data['children']:
         data_children = data_index['data']
-        print(data_children['title'])
+        title = data_children['title']
+        print(title)
